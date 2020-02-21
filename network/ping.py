a72d3f7b0df7bffa8223896793112bf3466bf02b
dnagy <nagydaniel1337@gmail.com>
2020-02-21 08:15:49 +0100

    ping liveoutput feature not finished yet

--- network/ping.py
+++ network/ping.py
@@ -4,41 +4,51 @@ import multiprocessing.dummy
 
 class Pinger:
 
-    def ping(self, ip):
+    @staticmethod
+    def ping(ip, liveoutput: bool = False):
         """
 
+        :param liveoutput:
         :param ip:
         :return:
         """
-        success = self.singleping(hostname=ip)  # Run the ping command here
-        if success:
-            print("{} responded".format(ip))
-        else:
-            print("{} did not respond".format(ip))
-        print(success)
-        return success
+        # get response time in ms or None if host not reached in 1 sec
+        success = Pinger.singleping(hostname=ip)  # Run the ping command here
+        if liveoutput:
+            print("Live output enabled:")
+            if success:
+                print("{} responded".format(ip))
+            else:
+                print("{} did not respond".format(ip))
+        return {ip: success}
+        # return success
 
-    def ping_range(self, network: str = "192.168.2.", start: int = 1, end: int = 254):
+    @staticmethod
+    def ping_range(network: str = "192.168.0.", start: int = 1, end: int = 254, liveoutput: bool = False):
         """
 
+        :param liveoutput:
         :param network:
         :param start:
         :param end:
         """
-
+        from itertools import product
         num_threads = 2 * multiprocessing.cpu_count()
-        p = multiprocessing.dummy.Pool(num_threads)
-        # for i in range(start,end)
+        # p = multiprocessing.dummy.Pool(num_threads)
+        # result = p.map(Pinger.ping, [network + str(x) for x in range(start, end)])
 
-        p.map(self.ping, [network + str(x) for x in range(start, end)])
+        with multiprocessing.Pool(processes=num_threads) as pool:
+            results = pool.starmap(Pinger.ping, product([network + str(x) for x in range(start, end)]), liveoutput)
+            # print(results)
+
+        return results
 
     @staticmethod
     def singleping(hostname):
         """
-
-                :param hostname:
-                :return:
-                """
+        :param hostname:
+        :return:
+        """
         output = subprocess.getoutput("timeout 1 ping -c 1 " + hostname).split(" ")
         for i in output:
             if "time=" in i:
@@ -47,5 +57,7 @@ class Pinger:
 
 
 if __name__ == "__main__":
-    p = Pinger()
-    p.ping_range()
+    print(Pinger.ping_range(network="192.168.0.", start=1, end=81, liveoutput=True))
+    # response = Pinger.ping("192.168.0.2")
+    # if response:
+    #     print(response)
